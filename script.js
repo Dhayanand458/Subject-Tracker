b1791250552b7a406a3eb7b027d0cf0a8c1a2322
Dhayanand458 <141307826+Dhayanand458@users.noreply.github.com>
2025-04-08 18:02:29 +0530

    script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const invisibleModeIcon = document.getElementById("invisible-mode-toggle");
 
   // Constants
-  const predefinedSubjects = ["Java", "Python", "C++", "HRM"];
+  const predefinedSubjects = ["Java", "ADA", "FOOS", "HRM", "principle of marketing - Minor", "Marketing (elective)"];
 
   // State Variables
   let subjects = [];
